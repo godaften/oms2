5d12e86f8e15e450d8335872184dbdb691711975
godaften <73276996+godaften@users.noreply.github.com>
2022-12-31 13:43:16 +0100

    added kontorhusefcorerepository

---
+++ OMS.Plugins/OMS.Plugins.EFCoreSqlServer/KontorhusEFCoreRepository.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OMS.CoreBusiness;
+using OMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OMS.Plugins.EFCoreSqlServer;
+
+public class KontorhusEFCoreRepository : IKontorhusRepository
+{
+    private readonly OMSContext db;
+
+    public KontorhusEFCoreRepository(OMSContext db)
+    {
+        this.db = db;
+    }
+
+    public async Task AddKontorhusAsync(Kontorhus kontorhus)
+    {
+        //this.db.Kontorhuse.Add(kontorhus);
+        //await this.db.SaveChangesAsync();
+
+        this.db.Kontorhuse.Add(kontorhus);
+        FlagLejereUnchanged(kontorhus, this.db);
+
+        await this.db.SaveChangesAsync();
+    }
+
+ 
+
+    public async Task<Kontorhus?> GetKontorhusById(int kontorhusId)
+    {
+        // include og theninclude... tjek op på
+        // Når vi finder det søgte kontorhus, vil vi også gerne se de relevante lejere
+        return await this.db.Kontorhuse.Include(x => x.KontorhusLejere)
+            .ThenInclude(x => x.Lejer)
+            .FirstOrDefaultAsync(x => x.KontorhusID== kontorhusId);
+    }
+
+
+
+    public async Task<IEnumerable<Kontorhus>> GetKontorhuseByNameAsync(string name)
+    {
+       return await this.db.Kontorhuse.Where (x => x.KontorhusNavn.ToLower().IndexOf(name) >= 0).ToListAsync();
+    }
+
+    public async Task UpdateKontorhusAsync(Kontorhus kontorhus)
+    {
+        // Undgå forskellige kontorhuse med samme navn
+        var khus = await this.db.Kontorhuse
+            .Include(x => x.KontorhusLejere)
+            .FirstOrDefaultAsync(x => x.KontorhusID == kontorhus.KontorhusID);
+
+        if (khus != null)
+        {
+            khus.KontorhusNavn = kontorhus.KontorhusNavn;
+            khus.KontorhusTelefon = kontorhus.KontorhusTelefon;
+            khus.KontorhusEmail = kontorhus.KontorhusEmail;
+            khus.KontorhusLejere = kontorhus.KontorhusLejere;
+            FlagLejereUnchanged(kontorhus, this.db);
+
+            await this.db.SaveChangesAsync();
+        }
+    }
+
+    // EF kender ikke til lejerne her, og vil derfor betragte tilføjede lejere som nye,
+    // selvom de findes i forvejen. Relevant ved samme lejer, der vil leje lokaler i flere kontorhuse.
+    // Her fortæller vi EF at alle lejerne eksisterer allerede og der skal derfor ikke gøres noget med dem
+    // Derfor laves nedenstående hjælpemetode
+    private void FlagLejereUnchanged(Kontorhus kontorhus, OMSContext db)
+    {
+        if (kontorhus?.KontorhusLejere != null &&
+                    kontorhus.KontorhusLejere.Count > 0)
+        {
+
+            foreach (var khusLej in kontorhus.KontorhusLejere)
+            {
+                if (khusLej.Lejer != null)
+                    db.Entry(khusLej.Lejer).State = EntityState.Unchanged;
+
+            }
+        }
+    }
+}
